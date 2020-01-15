a93a40c8fb66f125556caf85ba39081607cfc04b
maaz1996 <maaz.abdullah111@gmail.com>
2020-01-15 15:20:39 +0530

    feat: get,add,update,delete product API

--- demo/src/main/java/come/example/demo/DemoApplication.java
+++ demo/src/main/java/come/example/demo/DemoApplication.java
@@ -2,11 +2,12 @@ package come.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
---
+++ demo/src/main/java/come/example/demo/controller/ProductServiceController.java
@@ -0,0 +1,56 @@
+package come.example.demo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import come.example.demo.model.Product;
+
+@RestController
+public class ProductServiceController {
+    private static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        final Product honey = new Product();
+        honey.setId("1");
+        honey.setName("Honey");
+        productRepo.put(honey.getId(), honey);
+
+        final Product almond = new Product();
+        almond.setId("2");
+        almond.setName("Almond");
+        productRepo.put(almond.getId(), almond);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> delete(@PathVariable("id") final String id) {
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") final String id,
+            @RequestBody final Product product) {
+        productRepo.remove(id);
+        product.setId(id);
+        productRepo.put(id, product);
+        return new ResponseEntity<>("Product is updated successsfully", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody final Product product) {
+        productRepo.put(product.getId(), product);
+        return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getProduct() {
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+}
